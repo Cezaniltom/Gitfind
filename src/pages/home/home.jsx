14e10cb3ecbe06e19d65a28ecdc03b1a3b9ac112
Cezaniltom <cezaniltom@outlook.com>
2024-06-25 10:55:06 -0300

    Criando e estilizando o header da aplicação

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,8 @@
-import './App.css';
-
+import { Header } from '../../components/Header/header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
     </div>
   );
 }
