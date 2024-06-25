91cdc1923dc605bece73b81b5f54b14601251e66
Cezaniltom <cezaniltom@outlook.com>
2024-06-25 13:37:44 -0300

    Estilizando input e o button

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,22 @@
 import { Header } from '../../components/Header/header';
+import background from '../../assets/background.png';
+
+import './style.css'
+
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='conteudo'>
+        <img src={background} className='background' alt='background app'/>
+        <div className='info'>
+          <div>
+            <input name='usuario' placeholder='@username' />
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
