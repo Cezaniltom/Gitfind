716e827ab74a24d38c3e9eb4aeb985f4fa27972a
Cezaniltom <cezaniltom@outlook.com>
2024-06-25 09:48:03 -0300

    Inicializando projeto

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
 import './App.css';
->>>>>>> bd78a3dcb3e9c73b0a193ddd19f927c057625329
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      
-=======
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
->>>>>>> bd78a3dcb3e9c73b0a193ddd19f927c057625329
     </div>
   );
 }
