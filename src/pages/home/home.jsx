c3f49a434438e135fd5b2bf194247fb4b9ef57bf
Cezaniltom <cezaniltom@outlook.com>
2024-06-26 08:37:25 -0300

    Adicionando a lista de itens que serão integrados

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header/header';
 import background from '../../assets/background.png';
+import ItemList from '../../components/List/ItemList';
 
 import './style.css'
 
@@ -14,6 +15,21 @@ function App() {
           <div>
             <input name='usuario' placeholder='@username' />
             <button>Buscar</button>
+          </div>
+          <div className='perfil'>
+            <img src="https://avatars.githubusercontent.com/u/91354726?s=400&u=7564509044bfe2f42f2a441556ac6709923ad347&v=4" className='profile' alt='Imagem de perfil'/>
+            <div>
+              <h3>Cezaniltom Silva</h3>
+              <span>@Cezaniltom</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h4 className='repositorio'>Repositórios</h4>
+            <ItemList title="Teste1" description="teste de descrição"/>
+            <ItemList title="Teste1" description="teste de descrição"/>
+            <ItemList title="Teste1" description="teste de descrição"/>
           </div>
         </div>
       </div>
